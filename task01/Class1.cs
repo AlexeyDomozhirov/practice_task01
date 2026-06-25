c9420734d85439016c40907720f2eb34b68e2fcc
Alexei <alexei-2006@bk.ru>
2026-06-25 11:52:49 +0600

    First task implementation

---
+++ task01/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace task01;
+
+using System.Linq;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+	var filt_input = input.ToLower().Where(x => !char.IsPunctuation(x) && !char.IsWhiteSpace(x));
+	return filt_input.SequenceEqual(filt_input.Reverse());
+    }
+}
