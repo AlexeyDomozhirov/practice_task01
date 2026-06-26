18e24790f3a609e674b0a02182b8f1f35daf119a
Alexei <alexei-2006@bk.ru>
2026-06-26 13:59:48 +0600

    Implementation of second task

---
+++ task02/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace task02;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Faculty { get; set; }
+    public List<int> Grades { get; set; }
+}
+
+public class StudentService
+{
+    private readonly List<Student> _students;
+
+    public StudentService(List<Student> students) => _students = students;
+
+    public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+        => _students.Where(s => s.Faculty == faculty);
+
+    public IEnumerable<Student> GetStudentsWithMinAverageGrade(double minAverageGrade)
+        => _students.Where(s =>
+            (s.Grades?.Count > 0 ? s.Grades.Average() : 0.0) >= minAverageGrade);
+
+    public IEnumerable<Student> GetStudentsOrderedByName()
+        => _students.OrderBy(s => s.Name);
+
+    public ILookup<string, Student> GroupStudentsByFaculty()
+        => _students.ToLookup(s => s.Faculty);
+
+    public string GetFacultyWithHighestAverageGrade()
+        => _students
+            .GroupBy(s => s.Faculty)
+            .MaxBy(g => g
+                .SelectMany(s => s.Grades ?? Enumerable.Empty<int>())
+                .DefaultIfEmpty()
+                .Average()) ?.Key;
+}
